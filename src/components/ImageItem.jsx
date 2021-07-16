9bbe4952e2f28209d378ff4baa31c8b55ece55d7
Nina <>
2021-07-16 16:27:26 +0200

    Add likes counter functionality

--- src/components/ImageItem.jsx
+++ src/components/ImageItem.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 export default function ImageItem({imageItem}) {
+ 
+  let [likes, setLikes] = useState(imageItem.likes);
+
+  function handleOnClick() {
+    setLikes(likes + 1);
+  }
+
   return (
     <div className="col-md-6">
-      <img className="img-fluid" 
+      <img 
+        className="img-fluid" 
         src={imageItem.imageURL} 
         alt={imageItem.description}
       />
       <strong>{imageItem.title}</strong>
       <p>{imageItem.description}</p>
-      <p>Likes: {imageItem.likes}</p>
+      <p 
+        onClick={handleOnClick}>
+        ❤ {likes}
+      </p>
     </div>
   )
 }
