9cfc5ccc780eacd18b1fe163c5037807a07ad4ee
Nina <>
2021-07-21 17:42:17 +0200

    change from static data to fetching from api

--- src/pages/ImageListPage.jsx
+++ src/pages/ImageListPage.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ImageItem from '../components/ImageItem';
-import { imageList } from "../imageList";
 
 export default function ImageListPage() {
+
+  const [imageList, setImageList] = useState([])
+
+  function fetchImageList() {
+    fetch("https://image-mock-data.firebaseio.com/images.json")
+      .then(response => response.json())
+      .then(data => setImageList(data))
+  }
+
+  useEffect( ()=> {
+    fetchImageList()
+    }, [])
+
   return (
     <>
       <h2>Fakestagram</h2>
       <div className="row">
-        {imageList.map( (imageItem) => {
+        {imageList.map( (imageItem, index) => {
           return <ImageItem 
+          imageIndex={index}
             key={imageItem.id} 
             imageItem={imageItem}
           />
